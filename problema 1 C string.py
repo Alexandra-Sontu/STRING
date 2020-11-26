f5454d510163150c6229a9776e1f216905a1442c
Alexandra-Sontu <71020688+Alexandra-Sontu@users.noreply.github.com>
2020-11-26 19:57:13 +0200

    Add files via upload

---
+++ problema 1 C string.py
@@ -0,0 +1,6 @@
+a=str(input("dati n sir de caractere:"))
+n=0
+for i in a:
+    if((ord(i) in range(32,48))or(ord(i) in range(58,65)) or (ord(i) in range(91,9)) or (ord(i) in range (123,127))):
+        n+=1
+        print(n)
